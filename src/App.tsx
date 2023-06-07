55bc308e228d32916f028c175d0337655aafaf09
Bryan McCarthy <bryanmccarthy2@gmail.com>
2023-06-06 21:19:05 -0400

    initial cardset

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import './App.css';
+import { useState } from 'react';
+import cardset from './cardset';
 
 function App() {
+  const [cards, setCards] = useState(cardset);
+
   return (
     <div className="App">
       Memory Game
---
+++ src/cardset.ts
@@ -0,0 +1,22 @@
+const cardset = [
+  { id: 1, color: "#FF0000", flipped: false, matched: false },
+  { id: 2, color: "#00FF00", flipped: false, matched: false },
+  { id: 3, color: "#0000FF", flipped: false, matched: false },
+  { id: 4, color: "#00FFFF", flipped: false, matched: false },
+  { id: 5, color: "#FF00FF", flipped: false, matched: false },
+  { id: 6, color: "#FFFF00", flipped: false, matched: false },
+  { id: 7, color: "#FFA500", flipped: false, matched: false },
+  { id: 8, color: "#800080", flipped: false, matched: false },
+  { id: 9, color: "#FFC0CB", flipped: false, matched: false },
+  { id: 10, color: "#A52A2A", flipped: false, matched: false },
+  { id: 11, color: "#008080", flipped: false, matched: false },
+  { id: 12, color: "#00FF00", flipped: false, matched: false },
+  { id: 13, color: "#4B0082", flipped: false, matched: false },
+  { id: 14, color: "#800000", flipped: false, matched: false },
+  { id: 15, color: "#000080", flipped: false, matched: false },
+  { id: 16, color: "#808000", flipped: false, matched: false },
+  { id: 17, color: "#C0C0C0", flipped: false, matched: false },
+  { id: 18, color: "#FFD700", flipped: false, matched: false },
+];
+
+export default cardset;
